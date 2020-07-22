39d89647fd234be93540d4c37a273e54b81ea5a5
Chris Kay <kckateera@gmail.com>
2020-07-23 00:35:18 +0200

    Implement and test Spoonacular API

--- routes/explore.js
+++ routes/explore.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require("unirest");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('explore', { title: 'Express' });
 });
+
+
+var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+
+req.query({
+  "q": "How much vitamin c is in 2 oranges%3F"
+});
+
+req.headers({
+  "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  "x-rapidapi-key": "cdfd46f434mshdd74d0813ed3c69p1973cdjsnd2b2b667a055",
+  "useQueryString": true
+});
+
+
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
+
+  console.log(res.body);
+});
+
+
 
 module.exports = router;
