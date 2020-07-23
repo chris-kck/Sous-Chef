a9a0bd9a9e832cb914bb80a18a5346c3147255a1
Chris Kay <kckateera@gmail.com>
2020-07-23 14:47:05 +0200

    Add Featured Recipes

--- routes/cook-zone.js
+++ routes/cook-zone.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
-/* GET home page. */
+var recipes; //list with recipes
+
 router.get('/', function(req, res, next) {
-  res.render('cook-zone', { title: req.session.user });
+
+  var request = unirest("GET", "https://api.spoonacular.com/recipes/random");
+
+  request.query({ "apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "number": 5 });
+
+  request.headers({ "useQueryString": true });
+
+  request.end(function (response) {
+    if (response.error) throw new Error(response.error);
+    req.recipes = response.body.recipes;
+    console.log(req.recipes);
+    res.render('cook-zone', { title: req.session.user , recipes: req.recipes});
+  });
+
+  //console.log(req.recipes); //undefined? why
+  //next();
 });
+
+
+/*
+
+
+
+router.get('/', function(req, res, next) {
+  //console.log(req.recipes)
+  res.render('cook-zone', { title: req.session.user , recipes: req.recipes});
+});
+
+*/
 
 
 module.exports = router;
