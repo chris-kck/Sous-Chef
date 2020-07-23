192d7d9561135baa22354f162ee309369b5f5429
Chris Kay <kckateera@gmail.com>
2020-07-23 13:10:55 +0200

    Finish adding UI elements

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  req.session.username = req.body.username;
+  res.render('cook-zone', { username: req.session.username });
+});
+
 module.exports = router;
