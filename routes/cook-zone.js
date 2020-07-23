dadc751d84f0e08f1b3211c6894b3b0e764d30c9
Chris Kay <kckateera@gmail.com>
2020-07-23 21:05:16 +0200

    Fix async api call, use router req & next.

--- routes/cook-zone.js
+++ routes/cook-zone.js
@@ -2,38 +2,29 @@ var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
 
-var recipes; //list with recipes
+
+
+
+
 
 router.get('/', function(req, res, next) {
+    unirest
+        .get("https://api.spoonacular.com/recipes/random")
+        .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "number": 12})
+        .headers({"useQueryString": true})
+        .end(function (response) {
+            if (response.error) throw new Error(response.error);
+            req.recipes = response.body.recipes;
+            next();
+        });
+});
 
-  var request = unirest("GET", "https://api.spoonacular.com/recipes/random");
 
-  request.query({ "apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "number": 12 });
 
-  request.headers({ "useQueryString": true });
-
-  request.end(function (response) {
-    if (response.error) throw new Error(response.error);
-    req.recipes = response.body.recipes;
-    console.log(req.recipes);
+router.get('/', function(req, res) {
     res.render('cook-zone', { title: req.session.user , recipes: req.recipes});
-  });
-
-  //console.log(req.recipes); //undefined? why
-  //next();
 });
 
-
-/*
-
-
-
-router.get('/', function(req, res, next) {
-  //console.log(req.recipes)
-  res.render('cook-zone', { title: req.session.user , recipes: req.recipes});
-});
-
-*/
 
 
 module.exports = router;
