c67c81fce4d272b7148026362cb34db98fb5e32d
Chris Kay <kckateera@gmail.com>
2020-07-24 09:52:10 +0200

    Handle search post method, query, render results page

--- routes/explore.js
+++ routes/explore.js
@@ -6,10 +6,27 @@ var unirest = require("unirest");
 router.get('/', function(req, res, next) {
   res.render('explore', { username: req.session.user });
 });
+
 //Handle search query & render same page, but with results. Nunjucks is amazing :)
 router.post('/', function(req, res, next) {
+  unirest
+      .get("https://api.spoonacular.com/recipes/search")
+      .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "number": 20, "cuisine":req.body.cuisine,
+            "query":req.body.recipe, "diet":req.body.diet, "intolerances":req.body.allergens
+            })
+      .headers({"useQueryString": true})
+      .end(function (response) {
+        if (response.error) throw new Error(response.error);
+        req.searchRes = response.body.results;
+        next();
+      });
+});
+
+router.post('/', function(req, res, next) {
   //run api search query with supplied user parameters.
-  res.render('explore', { username: req.session.user });
+  //https://api.spoonacular.com/recipes/search?cuisine=<string>&diet=<string>&intolerances=<string>&number=<number>
+  console.log(req.body);
+  res.render('explore', { username: req.session.user , searchRes: req.searchRes});
 });
 /*
 
