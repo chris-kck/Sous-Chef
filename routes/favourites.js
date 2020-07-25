d9e3a9cf93e9b6faeea7691c10243af96540b9d0
Chris Kay <kckateera@gmail.com>
2020-07-25 19:30:40 +0200

    Implement retrieval of favourites via batch api and display.

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('favourites', { username: req.session.user });
+  //request information for fav recipes and return list of recipes marked as favourites
+  if (req.session.favourites != undefined){ //only call if favourites have been added.
+    var recipesCSV = req.session.favourites.join(",");
+
+    unirest
+        .get("https://api.spoonacular.com/recipes/informationBulk")
+        .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709","ids": recipesCSV })
+        .headers({"useQueryString": true})
+        .end(function (response) {
+          if (response.error) throw new Error(response.error);
+          console.log(response.body);
+          req.favRecipes = response.body;
+          res.render('favourites', { favRecipes: req.favRecipes });
+
+        });
+  }
+  else { res.render('favourites');}
 });
 
 
-//Handler for recipes added to favourites
+//Handler for recipes added to favourites via ajax call.
 router.post('/', function(req, res, next) {
   req.session.favourites =  req.session.favourites || []; //itself if it exists else empty list
   req.session.favourites.push( req.body.recID); //push to list of fav recipe objects for session
-  res.send("Your recipe has been successfully added");
+  res.send("Your recipe has been successfully bookmarked");
   console.log(req.session.favourites);
 });
 
