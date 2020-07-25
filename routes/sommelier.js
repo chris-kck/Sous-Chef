b3b90a6da85aec965e119402594a899f243470d2
Chris Kay <kckateera@gmail.com>
2020-07-25 22:40:51 +0200

    Add handlers for wine earch queries

--- routes/sommelier.js
+++ routes/sommelier.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('sommelier', { username: req.session.user });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.recipe);
+  if (req.body.recipe) {
+    unirest
+        .get("https://api.spoonacular.com/food/wine/pairing")
+        .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "food": req.body.recipe})
+        .headers({"useQueryString": true})
+        .end(function (response) {
+          if (response.error) throw new Error(response.error);
+          console.log(response.body); //results?
+          res.render('sommelier', { recipe:req.body.recipe, wineRes: response.body.pairedWines, wineText: response.body.pairingText });
+        });
+  }
+  else if (req.body.wine){
+    unirest
+        .get("https://api.spoonacular.com/food/wine/pairing")
+        .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709", "food": req.body.recipe})
+        .headers({"useQueryString": true})
+        .end(function (response) {
+          if (response.error) throw new Error(response.error);
+          req.searchRes = response.body.results; //results?
+          res.render('sommelier', { username: req.session.user });
+        });
+  }
+
 });
 
 module.exports = router;
