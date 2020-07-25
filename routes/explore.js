d81699d2f6f37523584e3594aae3ade9c7c9fcae
Chris Kay <kckateera@gmail.com>
2020-07-25 19:31:09 +0200

    Refactor code

--- routes/explore.js
+++ routes/explore.js
@@ -29,29 +29,5 @@ router.post('/', function(req, res, next) {
     console.log(req.searchRes);
   res.render('explore', { username: req.session.user , searchRes: req.searchRes});
 });
-/*
-
-var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
-
-req.query({
-  "q": "How much vitamin k is in spinach%3F"
-});
-
-req.headers({
-  "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  "x-rapidapi-key": "cdfd46f434mshdd74d0813ed3c69p1973cdjsnd2b2b667a055",
-  "useQueryString": true
-});
-
-
-
-
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
-
-  console.log(res.body);
-});
-
-*/
 
 module.exports = router;
