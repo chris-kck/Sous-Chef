379c07aeb27811c0c8a26037ac53aa08117c2b59
Chris Kay <kckateera@gmail.com>
2020-07-26 09:28:44 +0200

    Implement Random Trivia API on every page load

--- routes/explore.js
+++ routes/explore.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 var unirest = require("unirest");
 
+
+//Call Trivia despite http vert (post/get)
+router.all('/', function(req, res, next) {
+    unirest
+        .get("https://api.spoonacular.com/food/trivia/random")
+        .query({"apiKey": "20faf6bbe4074762be9f0c0db3fe9709"})
+        .headers({"useQueryString": true})
+        .end(function (response) {
+            if (response.error) throw new Error(response.error);
+            req.trivia = response.body.text;
+            console.log(req.trivia);
+            next();
+        });
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('explore', { username: req.session.user });
+  res.render('explore', { username: req.session.user, trivia: req.trivia });
 });
 
 //Handle search query & render same page, but with results. Nunjucks is amazing :)
@@ -22,12 +38,15 @@ router.post('/', function(req, res, next) {
       });
 });
 
+
+
 router.post('/', function(req, res, next) {
   //run api search query with supplied user parameters.
   //https://api.spoonacular.com/recipes/search?cuisine=<string>&diet=<string>&intolerances=<string>&number=<number>
   //console.log(req.body); //what's been searched
     console.log(req.searchRes);
-  res.render('explore', { username: req.session.user , searchRes: req.searchRes});
+  res.render('explore', { username: req.session.user , searchRes: req.searchRes, trivia: req.trivia});
 });
+
 
 module.exports = router;
